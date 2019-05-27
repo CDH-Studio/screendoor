c8e36f3e6ad3549716253618bc749d8c7c365cb3
Jared Ridyard <46499132+HeyaImPrizm@users.noreply.github.com>
2019-05-27 11:58:27 -0400

    Add debugger support (#9)

--- screendoor_app/wsgi.py
+++ screendoor_app/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'screendoor_app.settings')
 
 application = get_wsgi_application()
+
+from wdb.ext import WdbMiddleware
+application = WdbMiddleware(application)
