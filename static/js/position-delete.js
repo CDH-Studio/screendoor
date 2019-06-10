ba63982bf77abb0c497e9308afad7ed0e5027cda
Sam Heaton <heat0072@algonquinlive.com>
2019-06-10 08:37:50 -0400

    JS file and template cleanup
    Removed all inline JS where possible. Exception is where scripts
    depend on Django template language variables, which must be executed
    inline in the template file.
    
    Added missing comments to template files and deleted some unused .js files.

---
+++ static/js/position-delete.js
@@ -0,0 +1,5 @@
+ /* Initializes modal pop-up for delete position confirmation */
+ document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.modal');
+   var instances = M.Modal.init(elems, {} /* options */ );
+ });
