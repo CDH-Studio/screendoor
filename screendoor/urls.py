ec7d60bc01dae3a014e90baa12f955e2bbbbdd7b
Sam Heaton <heat0072@algonquinlive.com>
2019-06-11 12:44:07 -0400

    Fixed bug causing exception when user confirms account
    Exception was caused by the account validation function not returning
    the user object, so when the user email should be displayed in a
    confirmation message it was not found.

--- screendoor/urls.py
+++ screendoor/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('confirm/', views.login_form, name='confirm_account'),
     path('createnewposition/', views.import_position, name='importposition'),
     path('positions/', views.positions, name='positions'),
-    path('position/', views.position, name='position'),
+    path('position/', views.position_detail, name='position'),
     path('importapplications/', views.import_applications,
          name='importapplications'),
 ]
