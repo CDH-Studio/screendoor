b4283194cacd8a33990349aec907629345c8c198
Sam Heaton <44000390+heatonsam@users.noreply.github.com>
2019-06-19 10:14:59 -0400

    Sam celery (#59)
    * MultiValueDictKey error fixed and other fixes
    
    - MultiValueDictKey error exception will no longer occur when
      uploading application(s) from the position detail view.
    - The applications view will no longer display empty fields for
      Complementary Response if the applicant answered no, or for
      Complementary Question if the field has no content.
    - Line spacing, font size, and boldness has been adjusted for table
      headers and cells so that the headings are no longer so overwhelming
      and the cell text appears less crunched.
    
    * Fixed missing application form values for returned application view
    
    In the previous commit, after uploading an application, clicking the
    upload applications button again would result in a blank form as the
    form was not being returned with the view. This is now fixed.
    
    * Demo application view commit
    
    * Merging with Application View Fixes Branch
    
    * GET requests for postion, applicant; split views for edit, delete
    
    - Accessing positions and applicants are now done via GET requests.
    - The respective functions check not just the values passed but if the
    current user has access to the respective positions and applicants.
    - edit, delete, and upload applications are split into their own
    functions in views.py.
    - String literals in application.html and elsewhere replaced with variables
    
    * Testing for edit, delete positions, upload applications
    
    tests.py now contains successful unit tests for editing and deleting
    position functions and uploading applications.
    
    a couple of small mistakes in views.py were corrected as a result of
    the testing.
    
    * Added Celery and RabbitMQ
    
    Celery and RabbitMQ are now part of the application, along with
    Flower, a web-based interface to view Celery processes. After
    integrating these changes, the application should be rebuilt.
    
    There are now docker containers for a celery worker (async tasks),
    celery-beat (scheduled tasks), rabbitmq (message passing), and Flower.
    All are working except Flower which is not accepting connections at
    localhost:9000 despite its reporting that it is running.
    
    Two scheduled tasks are now implemented: deleting account
    authorization tokens if they have existed for two days or more, and
    deleting positions not associated with a user. Both are currently set
    to run once per day at midnight but have been tested using short intervals.

--- screendoor/admin.py
+++ screendoor/admin.py
@@ -5,19 +5,24 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import ScreenDoorUserCreationForm
 from .models import *
 
+
 class RequirementInline(admin.TabularInline):
     model = Requirement
     extra = 0
+
 
 class QuestionInline(admin.StackedInline):
     model = FormQuestion
     extra = 0
 
+
 class PositionAdmin(admin.ModelAdmin):
     inlines = [RequirementInline]
 
+
 class ApplicantAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
+
 
 admin.site.register(FormQuestion)
 admin.site.register(RequirementMet)
@@ -27,17 +32,24 @@ admin.site.register(Education)
 admin.site.register(Applicant, ApplicantAdmin)
 admin.site.register(Requirement)
 admin.site.register(Position, PositionAdmin)
+admin.site.register(EmailAuthenticateToken)
 
 
 class PositionInline(admin.TabularInline):
     model = ScreenDoorUser.positions.through
     extra = 0
 
+
+class AccountTokenInline(admin.TabularInline):
+    model = EmailAuthenticateToken
+    extra = 0
+
+
 class ScreenDoorUserAdmin(UserAdmin):
     add_form = ScreenDoorUserCreationForm
     model = ScreenDoorUser
     list_display = ['email', ]
-    inlines = [PositionInline]
+    inlines = [PositionInline, AccountTokenInline]
 
 
 admin.site.register(ScreenDoorUser, ScreenDoorUserAdmin)
