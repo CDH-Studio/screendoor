8c5b179c509683de9d1d8096d50c6818d2b9e642
Sam Heaton <44000390+heatonsam@users.noreply.github.com>
2019-06-25 12:45:30 -0400

    Application View JavaScript bugs fixed and refactoring (#71)
    * Can upload multiple files.
    
    * Updated log messages to reflect batches and applicants properly.
    
    * Application View JavaScript bugs fixed and refactoring
    
    tables-applicant.js is much cleaner and the following issues have been
    addressed in the Applicant view:
    
    - Collapse All button currently causes wrong text to appear on the Questions list
    - Analysis text in question detail view should also be blue
    
    * Removed unnecesary CSS lines
    
    * Streamlined lambdas for listeners
    
    * Fixed Expand All button bug
    
    - Fixed bug causing Expand All button to make education and quesiton
      headers glitch.

--- static/js/tables-applicant.js
+++ static/js/tables-applicant.js
@@ -36,210 +36,277 @@ let educationAreaStudyTruncated = [];
 /* Initializes positions table with experience and assets collapsed */
 window.addEventListener('DOMContentLoaded', (event) => {
   for (let i = 0; i < collapseElements.length; i++) {
+    initializeRowListeners(i);
     if (i > 3) {
       collapseArrows[i].innerHTML = "keyboard_arrow_right";
       collapseRow(collapseElements[i].nextElementSibling);
     }
   }
+  initializeEducationListeners();
+  initializeQuestionListeners();
 });
 
 /* Individual row click listeners */
-for (let i = 0; i < collapseElements.length; i++) {
+function initializeRowListeners(i) {
   collapseElements[i].style.cursor = 'pointer';
-  let rowToCollapse = collapseElements[i].nextElementSibling;
+  let row = collapseElements[i].nextElementSibling;
+
   collapseElements[i].addEventListener("click", function() {
-    collapseArrows[i].innerHTML == "keyboard_arrow_right" ? collapseArrows[i].innerHTML = "keyboard_arrow_down" : collapseArrows[i].innerHTML = "keyboard_arrow_right";
-    expandOrCollapseRows(rowToCollapse);
+    openCloseArrow(i);
+    expandOrCollapseRows(row);
   });
 }
 
 /* Collapse All button listener */
-collapseAllButton.addEventListener("click", function() {
-  collapseAll();
-});
+collapseAllButton.addEventListener("click", collapseAll);
 
-function collapseAll() {
-  for (let i = 0; i < collapseElements.length; i++) {
-    let rowToCollapse = collapseElements[i].nextElementSibling;
-    collapseArrows[i].innerHTML = "keyboard_arrow_right";
-    collapseRow(rowToCollapse);
-    questionHeaders[i].click();
+/* Expand all button listener */
+expandAllButton.addEventListener("click", expandAll);
+
+/* Initialize question listeners */
+function initializeQuestionListeners() {
+
+  /* Initializes and adds listeners for truncated question text */
+  for (let i = 0; i < questionEllipses.length; i++) {
+    previewFull[i] = document.getElementById("previews-full" + i);
+    previewSmall[i] = document.getElementById("previews-small" + i);
+    questionTruncated[i] = document.getElementById("truncated" + i);
+    questionTruncatedAnalysis[i] = document.getElementById("truncated-analysis" + i);
+    analysisSubheads[i] = document.getElementById("analysis-subhead" + i);
+    clickedHeaders[i] = document.getElementById("clicked-header" + i);
+
+    hideElement(clickedHeaders[i], previewFull[i]);
+    displayEllipsesIfNeeded(i);
+
+    /* User clicks question header */
+    questionHeaders[i].addEventListener("click", () => { expandCollapseQuestionHeaders(i); });
+
+    /* User moves mouse over a question ellipsis */
+    questionEllipses[i].addEventListener("mouseover", () => { showQuestionPreview(i); });
+
+    /* User moves mouse off a question ellipsis */
+    questionHeaders[i].addEventListener("mouseleave", () => { hideQuestionPreview(i); });
+
+    /* Resize adjustment for ellipses */
+    window.addEventListener('resize', () => { displayEllipsesIfNeeded(i); });
   }
 }
 
-/* Expand all button listener */
-expandAllButton.addEventListener("click", function() {
-  expandAll();
-});
+/* Initializes and creates listeners for education header truncation */
+function initializeEducationListeners() {
+  for (let i = 0; i < educationEllipses.length; i++) {
+    educationAcademicTruncated[i] = document.getElementById("education-academic-truncated" + i);
+    educationInstitutionTruncated[i] = document.getElementById("education-institution-truncated" + i);
+    educationAreaStudyTruncated[i] = document.getElementById("education-areastudy-truncated" + i);
 
+    if (!isEllipsisActive(educationAreaStudyTruncated[i]) &&
+        !isEllipsisActive(educationInstitutionTruncated[i]) &&
+        !isEllipsisActive(educationAcademicTruncated[i])) {
+      hideElement(educationEllipses[i]);
+    }
+
+    /* User clicks to show education detail */
+    educationHeaders[i].addEventListener("click", () => { showEducationFull(i); });
+
+    /* User clicks to hide education detail */
+    educationHeadersClicked[i].addEventListener("click", () => { hideEducationFull(i); });
+
+    /* User moves mouse over education ellipsis */
+    educationEllipses[i].addEventListener("mouseover", () => { showEducationPreview(i); });
+
+    /* User moves mouse off education ellipsis */
+    educationHeaders[i].addEventListener("mouseleave", () => { hideEducationPreview(i); });
+
+    /* Handles window resizing and adding/removing ellipses based on browser window size */
+    window.addEventListener('resize', () => { displayEllipsesIfNeeded(i); });
+  }
+}
+
+/* HELPER FUNCTIONS */
+
+/* Display ellipses if there is truncated text on row */
+function displayEllipsesIfNeeded(i) {
+  !isEllipsisActive(questionTruncated[i])
+    && !isEllipsisActive(questionTruncatedAnalysis[i]) ? hideElement(questionEllipses[i]) : showElement(questionEllipses[i]);
+}
+
+/* Arrow indicates row is open or closed */
+function openCloseArrow(i) {
+  collapseArrows[i].innerHTML == "keyboard_arrow_right" ? collapseArrows[i].innerHTML = "keyboard_arrow_down" : collapseArrows[i].innerHTML = "keyboard_arrow_right";
+}
+
+/* User wants to view question detail */
+function expandCollapseQuestionHeaders(i) {
+  if (!isOpen(previewSmall[i])) {
+    expandQuestionHeaders(i);
+  } else {
+    collapseQuestionHeaders(i);
+  }
+}
+
+function expandQuestionHeaders(i) {
+  showElement(clickedHeaders[i]);
+  hideElement(previewFull[i], previewSmall[i]);
+  backgroundOffWhite(questionHeaders[i]);
+}
+
+function collapseQuestionHeaders(i) {
+  showElement(previewSmall[i]);
+  hideElement(clickedHeaders[i]);
+  backgroundWhite(questionHeaders[i]);
+  hideQuestionPreview(i);
+}
+
+/* User moves mouse over question ellipses */
+function showQuestionPreview(i) {
+  hideElement(analysisSpan[i], questionSpan[i]);
+  showElement(previewFull[i], questionSubheads[i]);
+  backgroundOffWhite(previewSmall[i]);
+  unTruncate(questionTruncated[i]);
+  growEllipsis(questionEllipses[i]);
+
+  if (analysisSubheads[i]) {
+    showElement(analysisSubheads[i]);
+  }
+  if (questionTruncatedAnalysis[i]) {
+    unTruncate(questionTruncatedAnalysis[i]);
+  }
+}
+
+/* User moves mouse off question preview */
+function hideQuestionPreview(i) {
+  showElement(analysisSpan[i], questionSpan[i]);
+  hideElement(questionSubheads[i], previewFull[i]);
+  backgroundWhite(previewSmall[i]);
+  truncate(questionTruncated[i]);
+  shrinkEllipsis(questionEllipses[i]);
+
+  if (analysisSubheads[i]) {
+    hideElement(analysisSubheads[i]);
+  }
+  if (questionTruncatedAnalysis[i]) {
+    truncate(questionTruncatedAnalysis[i]);
+  }
+}
+
+/* User clicks an education row */
+function showEducationFull(i) {
+  showElement(educationHeadersClicked[i]);
+  hideElement(educationHeaders[i]);
+}
+
+/* User clicks an expanded education row */
+function hideEducationFull(i) {
+  hideElement(educationHeadersClicked[i]);
+  showElement(educationHeaders[i]);
+}
+
+/* User moves mouse over education ellipses */
+function showEducationPreview(i) {
+  if (isEllipsisActive(educationAreaStudyTruncated[i]) ||
+      isEllipsisActive(educationInstitutionTruncated[i]) ||
+      isEllipsisActive(educationAcademicTruncated[i])) {
+    growEllipsis(educationEllipses[i]);
+  }
+  backgroundOffWhite(educationHeaders[i]);
+  unTruncate(educationAcademicTruncated[i], educationInstitutionTruncated[i], educationAreaStudyTruncated[i]);
+}
+
+/* User moves mouse off education preview */
+function hideEducationPreview(i) {
+  backgroundWhite(educationHeaders[i]);
+  shrinkEllipsis(educationEllipses[i]);
+  truncate(educationAcademicTruncated[i], educationAreaStudyTruncated[i], educationInstitutionTruncated[i]);
+}
+
+/* Row is currently expanded */
+function isOpen(row) {
+  return row.classList.contains("hide");
+}
+
+/* Recursively expands successive TBODY rows */
+function expandRow(row) {
+  row.classList.remove("hide");
+  try {
+    if (row.nextElementSibling.tagName == "TBODY") {
+      row = row.nextElementSibling;
+      expandRow(row);
+    }
+  } catch (TypeError) {}
+}
+
+/* Recursively collapses successive TBODY rows */
+function collapseRow(row) {
+  row.classList.add("hide");
+  try {
+    if (row.nextElementSibling.tagName == "TBODY") {
+      row = row.nextElementSibling;
+      collapseRow(row);
+    }
+  } catch (TypeError) {}
+}
+
+/* Toggles TBODY collapse and expand */
+function expandOrCollapseRows(row) {
+  row.classList.contains("hide") ? row.classList.remove("hide") : row.classList.add("hide");
+  try {
+    if (row.nextElementSibling.tagName == "TBODY") {
+      row = row.nextElementSibling;
+      expandOrCollapseRows(row);
+    }
+  } catch (TypeError) {}
+}
+
+/* Collapse all rows */
+function collapseAll() {
+  for (let i = 0; i < collapseElements.length; i++) {
+    let row = collapseElements[i].nextElementSibling;
+    collapseArrows[i].innerHTML = "keyboard_arrow_right";
+    collapseRow(row);
+    for (let i = 0; i < questionHeaders.length; i++) {
+      collapseQuestionHeaders(i);
+    }
+    for (let i = 0; i < educationHeaders.length; i++) {
+      hideEducationFull(i);
+    }
+  }
+}
+
+/* Expand all rows */
 function expandAll() {
   for (let i = 0; i < collapseElements.length; i++) {
     let rowToExpand = collapseElements[i].nextElementSibling;
     collapseArrows[i].innerHTML = "keyboard_arrow_down";
     expandRow(rowToExpand);
   }
-}
-
-function showPreviewContents(preview, analysis, question) {
-  preview.style.display = "table-row";
-  analysis.style.display = "none";
-  question.style.display = "none";
-}
-
-function hidePreviewContents(preview, analysis, question) {
-  preview.style.display = "none";
-  analysis.style.display = "inline";
-  question.style.display = "inline";
-}
-
-/* Initializes and adds listeners for truncated question text */
-for (let i = 0; i < questionEllipses.length; i++) {
-  previewFull[i] = document.getElementById("previews-full" + i);
-  previewFull[i].style.display = "None";
-  previewSmall[i] = document.getElementById("previews-small" + i);
-  questionTruncated[i] = document.getElementById("truncated" + i);
-  questionTruncatedAnalysis[i] = document.getElementById("truncated-analysis" + i);
-  analysisSubheads[i] = document.getElementById("analysis-subhead" + i);
-  clickedHeaders[i] = document.getElementById("clicked-header" + i);
-  questionHeaders[i].isOpen = false;
-  hideElement(clickedHeaders[i]);
-
-  !isEllipsisActive(questionTruncated[i])
-                           && !isEllipsisActive(questionTruncatedAnalysis[i]) ? hideElement(questionEllipses[i]) :
-                              showElement(questionEllipses[i])
-
-  /* User clicks question header */
-  questionHeaders[i].addEventListener("click", function() {
-    if (!questionHeaders[i].isOpen) {
-      showElement(clickedHeaders[i]);
-      hideElement(previewFull[i]);
-      hideElement(previewSmall[i]);
-      clickedHeaders[i].style.display = "table-row";
-      questionHeaders[i].isOpen = true;
-    } else {
-      hideElement(clickedHeaders[i]);
-      showElement(previewSmall[i]);
-      showElement(previewFull[i]);
-      clickedHeaders[i].style.display = "none";
-      questionHeaders[i].isOpen = false;
-    }
-  });
-
-  /* User moves mouse over a question ellipsis */
-  questionEllipses[i].addEventListener("mouseover", function() {
-    showPreviewContents(previewFull[i], analysisSpan[i], questionSpan[i]);
-    backgroundOffWhite(previewSmall[i]);
-    showElement(questionSubheads[i]);
-    unTruncate(questionTruncated[i]);
-    growEllipsis(questionEllipses[i]);
-
-    if (analysisSubheads[i]) {
-      showElement(analysisSubheads[i]);
-    }
-    if (questionTruncatedAnalysis[i]) {
-      unTruncate(questionTruncatedAnalysis[i]);
-    }
-  });
-
-  /* User moves mouse off a question ellipsis */
-  questionHeaders[i].addEventListener("mouseleave", function() {
-    hidePreviewContents(previewFull[i], analysisSpan[i], questionSpan[i]);
-    backgroundWhite(previewSmall[i]);
-    hideElement(questionSubheads[i]);
-    shrinkEllipsis(questionEllipses[i]);
-    truncate(questionTruncated[i]);
-
-    if (analysisSubheads[i]) {
-      hideElement(analysisSubheads[i]);
-    }
-    if (questionTruncatedAnalysis[i]) {
-      truncate(questionTruncatedAnalysis[i]);
-    }
-  });
-}
-
-/* Initializes and creates listener for education header truncation */
-for (let i = 0; i < educationEllipses.length; i++) {
-  educationAcademicTruncated[i] = document.getElementById("education-academic-truncated" + i);
-  educationInstitutionTruncated[i] = document.getElementById("education-institution-truncated" + i);
-  educationAreaStudyTruncated[i] = document.getElementById("education-areastudy-truncated" + i);
-  educationHeaders[i].isOpen = false;
-
-  if (!isEllipsisActive(educationAreaStudyTruncated[i]) &&
-      !isEllipsisActive(educationInstitutionTruncated[i]) &&
-      !isEllipsisActive(educationAcademicTruncated[i])) {
-    hideElement(educationEllipses[i]);
+  for (let i = 0; i < questionHeaders.length; i++) {
+    expandQuestionHeaders(i);
   }
-
-  educationHeaders[i].addEventListener("click", function() {
-    showElement(educationHeadersClicked[i]);
-    hideElement(educationHeaders[i]);
-    educationHeadersClicked[i].style.display = "table-row";
-  });
-
-  educationHeadersClicked[i].addEventListener("click", function() {
-    hideElement(educationHeadersClicked[i]);
-    showElement(educationHeaders[i]);
-    clickedHeaders[i].style.display = "none";
-    educationHeaders[i].isOpen = false;
-  });
-
-  /* User moves mouse over education ellipsis */
-  educationEllipses[i].addEventListener("mouseover", function() {
-    if (isEllipsisActive(educationAreaStudyTruncated[i]) ||
-        isEllipsisActive(educationInstitutionTruncated[i]) ||
-        isEllipsisActive(educationAcademicTruncated[i])) {
-      growEllipsis(educationEllipses[i]);
-    }
-    backgroundOffWhite(educationHeaders[i]);
-    unTruncate(educationAcademicTruncated[i]);
-    unTruncate(educationInstitutionTruncated[i]);
-    unTruncate(educationAreaStudyTruncated[i]);
-  });
-
-  /* User moves mouse off education ellipsis */
-  educationHeaders[i].addEventListener("mouseleave", function() {
-    backgroundWhite(educationHeaders[i]);
-    shrinkEllipsis(educationEllipses[i]);
-    truncate(educationAcademicTruncated[i]);
-    truncate(educationAreaStudyTruncated[i]);
-    truncate(educationInstitutionTruncated[i]);
-  });
+  for (let i = 0; i < educationHeaders.length; i++) {
+    showEducationFull(i);
+  }
 }
-
-/* Handles window resizing and adding/removing ellipses based on browser window size */
-window.addEventListener('resize', function() {
-  for (let i = 0; i < questionEllipses.length; i++) {
-    !isEllipsisActive(questionTruncated[i])
-    && !isEllipsisActive(questionTruncatedAnalysis[i]) ? hideElement(questionEllipses[i]) : showElement(questionEllipses[i]);
-  }
-  for (let i = 0; i < educationEllipses.length; i++) {
-    !isEllipsisActive(educationAreaStudyTruncated[i]) && !isEllipsisActive(educationInstitutionTruncated[i]) && !isEllipsisActive(educationAcademicTruncated[i]) ? hideElement(educationEllipses[i]) : showElement(educationEllipses[i]);
-  }
-});
-
-/* HELPER FUNCTIONS */
 
 /* Hide an HTML element */
-function hideElement(element) {
-  element.classList.add("hide");
+function hideElement(...elements) {
+  elements.forEach(element => element.classList.add("hide"));
 }
 
 /* Show an HTML element */
-function showElement(element) {
-  element.classList.remove("hide");
+function showElement(...elements) {
+  elements.forEach(element => element.classList.remove("hide"));
 }
 
 /* Truncate the text in an HTML element */
-function truncate(element) {
-  element.classList.add("truncation");
-  element.classList.remove("truncation-open");
+function truncate(...elements) {
+  elements.forEach(element => element.classList.add("truncation"));
+  elements.forEach(element => element.classList.remove("truncation-open"));
 }
 
 /* Un-truncate the text in an HTML element */
-function unTruncate(element) {
-  element.classList.remove("truncation");
-  element.classList.add("truncation-open");
+function unTruncate(...elements) {
+  elements.forEach(element => element.classList.remove("truncation"));
+  elements.forEach(element => element.classList.add("truncation-open"));
 }
 
 /* Indicate that an ellipsis has mouse over it */
@@ -270,37 +337,5 @@ function isEllipsisActive(e) {
   if (e) {
     return (e.offsetWidth < e.scrollWidth);
   }
-}
-
-/* Recursively expands successive TBODY rows */
-function expandRow(rowToExpand) {
-  rowToExpand.style.display = 'table-row-group';
-  try {
-    if (rowToExpand.nextElementSibling.tagName == "TBODY") {
-      rowToExpand = rowToExpand.nextElementSibling;
-      expandRow(rowToExpand);
-    }
-  } catch (TypeError) {}
-}
-
-/* Recursively collapses successive TBODY rows */
-function collapseRow(rowToCollapse) {
-  rowToCollapse.style.display = 'none';
-  try {
-    if (rowToCollapse.nextElementSibling.tagName == "TBODY") {
-      rowToCollapse = rowToCollapse.nextElementSibling;
-      collapseRow(rowToCollapse);
-    }
-  } catch (TypeError) {}
-}
-
-/* Toggles TBODY collapse and expand */
-function expandOrCollapseRows(rowToCollapse) {
-  rowToCollapse.style.display == 'none' ? rowToCollapse.style.display = 'table-row-group' : rowToCollapse.style.display = 'none';
-  try {
-    if (rowToCollapse.nextElementSibling.tagName == "TBODY") {
-      rowToCollapse = rowToCollapse.nextElementSibling;
-      expandOrCollapseRows(rowToCollapse);
-    }
-  } catch (TypeError) {}
+  return false;
 }
