025096fab98e6188a54c998b9bb1cf7cd17c6d94
Sam Heaton <heat0072@algonquinlive.com>
2019-06-28 13:40:40 -0400

    Staged missing JavaScript files

---
+++ static/js/applicant-upload.js
@@ -0,0 +1,101 @@
+/* DOM constants */
+const uploadCard = document.getElementById("upload-applications-modal");
+const progressBlock = document.getElementById("progress-div");
+const currentNumberSpan = document.getElementById("current-number");
+const totalNumberSpan = document.getElementById("total-number");
+const progressBar = document.getElementById("progress-bar");
+const progressText = document.getElementById("progress-text");
+const loadingEllipses = document.getElementById("loading-ellipses");
+
+/* Constants derived from Django variables in hidden inputs */
+const queryUrl = new URL(document.getElementById("task-url").value, "http://localhost:8000");
+const reloadUrl = document.getElementById("reload-url").value;
+const taskId = document.getElementById("task-id").value;
+
+/* Variable representing ajax request timer */
+let updateTimer = null;
+
+let updateProgressText = function(total, current) {
+  progressText.innerHTML = document.getElementById("progress-text-value").value;
+  document.getElementById("current-number").innerHTML = current;
+  document.getElementById("total-number").innerHTML = total;
+};
+
+let updateLoadingBarProgress = function(total, current) {
+  progressBar.style.width = Math.floor(current * 100 / total) + "%";
+};
+
+let showEllipses = function() {
+  loadingEllipses.classList.add("loading");
+};
+
+let hideEllipses = function() {
+  loadingEllipses.classList.remove("loading");
+};
+
+let showProgressBar = function() {
+  progressBlock.classList.remove("hide");
+};
+
+let hideProgressBar = function() {
+  progressBlock.classList.add("hide");
+};
+
+let updateProgress = function(state, meta) {
+  let total = meta.total;
+  let current = meta.current;
+  updateLoadingBarProgress(total, current);
+  updateProgressText(total, current);
+};
+
+/* Display error message on applicant upload modal */
+let displayError = function() {
+  console.error();
+  clearInterval(updateTimer);
+  progressText.innerHTML = document.getElementById("upload-error-text").value;
+};
+
+/* Define what message displays on the applicant uploading modal */
+let displayProgress = function(queryUrl) {
+  fetch(queryUrl).then(function(response) {
+    /* data being the json object returned from Django function */
+    response.json().then(function(data) {
+      if (data.state == "PENDING") {
+        progressText.innerHTML = document.getElementById("calculating-applicants-text").value + ": " + data.meta.total;
+      } else if (data.state == "PROGRESS") {
+        showProgressBar();
+        updateProgress(data.state, data.meta);
+      } else if (data.state == "SUCCESS") {
+        clearInterval(updateTimer);
+        uploadModal.close();
+        window.location.assign(reloadUrl);
+      } else if (data.state == "FAILURE") {
+        displayError();
+      }
+    }).catch(error => console.error());
+  });
+};
+
+/* Execute and run timer if applicant file upload is taking place */
+let initializeApplicantUploadProgress = function() {
+  document.getElementById('files-processing').innerHTML = localStorage.getItem('applicationFiles');
+  clearExceptSidebar();
+  uploadModal.openInstant();
+  try {
+    displayProgress(queryUrl.href);
+    updateTimer = setInterval(function() {
+      displayProgress(queryUrl.href);
+    }, 1000);
+  } catch (e) {
+    clearInterval(updateTimer);
+  }
+};
+
+/* Show upload progress if there is a valid task ID */
+window.addEventListener("load", function() {
+  if (document.getElementById("task-id") && document.getElementById("task-id").value != "None") {
+    initializeApplicantUploadProgress();
+  } else {
+    hideProgressBar();
+  }
+});
---
+++ static/js/localstorage.js
@@ -0,0 +1,93 @@
+/* Clears local storage except the current sidebar status */
+const clearExceptSidebar = function() {
+  let tempSidebarStatus = JSON.parse(localStorage.getItem('sidenavOpen'));
+  localStorage.clear();
+  localStorage.setItem('sidenavOpen', tempSidebarStatus);
+};
+
+/* Display persisted form information, or if none exists, hide PDF and URL forms */
+const initializePositionImportVariables = function() {
+  if (localStorage.getItem('pdfRequired') === null) {
+    document.getElementById('pdf_upload_form').style.display = 'none';
+    document.getElementById('url_upload_form').style.display = 'none';
+    document.getElementById('position_submit_button').style.display = 'none';
+    document.getElementById('radio_pdf').checked = false;
+    document.getElementById('radio_url').checked = false;
+  } else {
+    document.getElementById('pdf_input').required = localStorage.getItem('pdfRequired');
+    document.getElementById('url_input').required = !localStorage.getItem('pdfRequired');
+    document.getElementById('pdf_path_input').value = localStorage.getItem('pdfText');
+    document.getElementById('pdf_input').value = null;
+    document.getElementById('url_input').value = localStorage.getItem('urlText');
+    document.getElementById('pdf_upload_form').style.display = localStorage.getItem('pdfDisplay');
+    document.getElementById('url_upload_form').style.display = localStorage.getItem('urlDisplay');
+    document.getElementById('radio_pdf').checked = localStorage.getItem('pdfChecked');
+    document.getElementById('radio_url').checked = !localStorage.getItem('pdfChecked');
+    clearExceptSidebar();
+  }
+};
+
+/* Persist uploaded file names to display alongside applicant processing bar */
+const persistPdfNames = function() {
+  localStorage.setItem('applicationFiles', document.getElementById('pdf_path_input').value);
+};
+
+/* Persist the form data to display alongside processed position */
+const persistUploadForm = function() {
+  localStorage.setItem('pdfRequired', document.getElementById('pdf_input').required);
+  localStorage.setItem('pdfText', document.getElementById('pdf_path_input').value);
+  localStorage.setItem('urlText', document.getElementById('url_input').value);
+  localStorage.setItem('pdfDisplay', document.getElementById('pdf_upload_form').style.display);
+  localStorage.setItem('urlDisplay', document.getElementById('url_upload_form').style.display);
+  localStorage.setItem('pdfChecked', document.getElementById('radio_pdf').checked);
+  localStorage.setItem('urlChecked', document.getElementById('radio_url').checked);
+};
+
+/* Show URL form, hide and clear PDF input form */
+const showUrl = function() {
+  document.getElementById('pdf_upload_form').style.display = 'none';
+  document.getElementById('pdf_input').required = false;
+  document.getElementById('pdf_input').value = null;
+  document.getElementById('pdf_path_input').value = null;
+  document.getElementById('url_upload_form').style.display = 'block';
+  document.getElementById('url_input').required = true;
+  document.getElementById('position_submit_button').style.display = 'block';
+  document.getElementById('position_submit_button').className = 'right btn disabled';
+  document.getElementById('position_submit_button').value = 'Coming soon';
+};
+
+/* Show PDF form, hide and clear URL input form */
+const showPdf = function() {
+  document.getElementById('pdf_upload_form').style.display = 'block';
+  document.getElementById('pdf_input').required = true;
+  document.getElementById('url_upload_form').style.display = 'none';
+  document.getElementById('url_input').required = false;
+  document.getElementById('url_input').value = null;
+  document.getElementById('position_submit_button').style.display = 'block';
+  document.getElementById('position_submit_button').className = 'right btn';
+  document.getElementById('position_submit_button').value = 'Submit';
+};
+
+/* Displays loading bar for position upload */
+const displayLoadingBar = function() {
+  document.getElementById("loading-bar").classList.remove("hide");
+};
+
+/* Checks for current page and initializes listeners */
+window.addEventListener("load", function() {
+  if (window.location.pathname.includes("/position") || window.location.pathname.includes("/positions")) {
+    document.getElementById("upload-applications-form").addEventListener("submit", persistPdfNames);
+  } else if (window.location.pathname.includes("/createnewposition")) {
+    initializePositionImportVariables();
+    document.getElementById("radio_pdf").addEventListener("click", showPdf);
+    document.getElementById("radio_url").addEventListener("click", showUrl);
+    document.getElementById("position_submit_button").addEventListener("click", function() {
+      displayLoadingBar();
+      persistUploadForm();
+    });
+    if (document.getElementById("save-button")) {
+      document.getElementById("save-button").addEventListener("clock", displayLoadingBar);
+    }
+    document.getElementById("loading-bar").classList.add("hide");
+  }
+});
