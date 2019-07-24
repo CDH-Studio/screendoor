d8029dff28023805c9e1a57a7e4476df9149bf4b
Sam Heaton <heat0072@algonquinlive.com>
2019-07-24 11:08:57 -0400

    Fixed bug with clicking questions in applicant view

--- static/js/applicant-questions.js
+++ static/js/applicant-questions.js
@@ -1,27 +1,26 @@
+const addNoteIcons = document.getElementsByClassName("add-note");
 const applicantResponseFull = document.getElementsByClassName("applicant-response-full");
 
+const educationItems = document.getElementsByClassName("education-item");
+const educationHeaders = document.getElementsByClassName("education-header");
+const extractFull = document.getElementsByClassName("extracts-full");
 const extractPreviews = document.getElementsByClassName("extract-previews");
 
-const extractFull = document.getElementsByClassName("extracts-full");
+const hiddenEducationInfo = document.getElementsByClassName("hidden-education-info");
+
+const questionPreviews = document.getElementsByClassName("question-preview");
+const questionPreviewDivs = document.getElementsByClassName("question-preview-div");
+const questionIcons = document.getElementsByClassName("question-icon-div");
+const questionAnswerFull = document.getElementsByClassName("question-answer-full");
 
 const requirementAbbreviations = document.getElementsByClassName("requirement-abbreviation");
 const requirementTips = document.getElementsByClassName("requirement-text");
 
-const questionPreviews = document.getElementsByClassName("question-preview");
-const questionPreviewDivs = document.getElementsByClassName("question-preview-div");
-
-const addNoteIcons = document.getElementsByClassName("add-note");
-
 const shortQuestionTexts = document.getElementsByClassName("short-question-text");
-
-const questionIcons = document.getElementsByClassName("question-icon-div");
-
-const questionAnswerFull = document.getElementsByClassName("question-answer-full");
-
-const educationHeaders = document.getElementsByClassName("education-header");
 
 let abbreviations = [];
 let descriptions = [];
+
 
 const initializeText = function(i) {
   abbreviations[i] = requirementAbbreviations[i].innerText;
@@ -34,6 +33,7 @@ const expandRequirementTip = function(i) {
   for (let j = 0; j < descriptionChars.length; j++) {
     requirementAbbreviations[i].innerText += descriptionChars[j];
   }
+
   requirementAbbreviations[i].addEventListener('transitionend', () => {
     requirementAbbreviations[i].classList.add("no-truncation");
   });
@@ -61,52 +61,79 @@ const truncateEducationHeader = function(i) {
 
 const expandEducationHeaders = function(i) {
   if (hiddenEducationInfo[i].classList.contains("row-closed")) {
+    educationItems[i].classList.remove("hoverable");
     hiddenEducationInfo[i].classList.remove("row-closed");
   } else {
+    educationItems[i].classList.add("hoverable");
     hiddenEducationInfo[i].classList.add("row-closed");
   }
 };
 
-const hiddenEducationInfo = document.getElementsByClassName("hidden-education-info");
+const openQuestionFull = function(i) {
+  questionPreviewDivs[i].classList.remove("hoverable");
+  applicantResponseFull[i].classList.add("applicant-response-full-open");
+  extractFull[i].classList.add("extracts-full-open");
+  questionAnswerFull[i].classList.remove("row-closed");
+  questionIcons[i].classList.add("hide");
+
+  for (let j = 0; j < extractFull[i].getElementsByTagName("i").length; j++) {
+    extractFull[i].getElementsByTagName("i")[j].classList.remove("hide");
+  }
+};
+
+const closeQuestionFull = function(i) {
+  applicantResponseFull[i].classList.remove("applicant-response-full-open");
+  extractFull[i].classList.remove("extracts-full-open");
+  questionPreviewDivs[i].classList.add("hoverable");
+  questionIcons[i].classList.remove("hide");
+  questionAnswerFull[i].classList.add("row-closed");
+
+  for (let j = 0; j < extractFull[i].getElementsByTagName("i").length; j++) {
+    extractFull[i].getElementsByTagName("i")[j].classList.add("hide");
+  }
+};
 
 const openCloseQuestionFull = function(i) {
   if (!applicantResponseFull[i].classList.contains("applicant-response-full-open")) {
-    questionPreviewDivs[i].classList.remove("hoverable");
-    applicantResponseFull[i].classList.add("applicant-response-full-open");
-    extractFull[i].classList.add("extracts-full-open");
-
-    for (let j = 0; j < extractFull[i].getElementsByTagName("i").length; j++) {
-      extractFull[i].getElementsByTagName("i")[j].classList.remove("hide");
-    }
-
-    questionAnswerFull[i].classList.remove("row-closed");
-    questionIcons[i].classList.add("hide");
+    openQuestionFull(i);
   } else {
-    applicantResponseFull[i].classList.remove("applicant-response-full-open");
-    extractFull[i].classList.remove("extracts-full-open");
-    questionPreviewDivs[i].classList.add("hoverable");
-
-    for (let j = 0; j < extractFull[i].getElementsByTagName("i").length; j++) {
-      extractFull[i].getElementsByTagName("i")[j].classList.add("hide");
-    }
-
-    questionIcons[i].classList.remove("hide");
-    questionAnswerFull[i].classList.add("row-closed");
+    closeQuestionFull(i);
   }
 };
 
 window.addEventListener('DOMContentLoaded', (event) => {
 
-  document.getElementsByTagName("html")[0].style.paddingBottom = "-5000px";
-
   for (let i = 0; i < questionPreviews.length; i++) {
-    questionPreviewDivs[i].addEventListener("click", () => {
-      openCloseQuestionFull(i);
+    shortQuestionTexts[i].addEventListener("mouseleave", () => {
+      extractPreviews[i].classList.remove("extract-extra-margin");
+    });
+
+    questionIcons[i].addEventListener("mouseover", () => {
+      extractPreviews[i].classList.add("extract-previews-open");
+      questionIcons[i].style.fontSize = "3rem";
+      if (requirementAbbreviations[i]) {
+        requirementAbbreviations[i].classList.add("hide");
+      }
+    });
+
+    questionPreviews[i].addEventListener("mouseleave", () => {
       extractPreviews[i].classList.remove("extract-previews-open");
       questionIcons[i].style.fontSize = "1.8rem";
       if (requirementAbbreviations[i]) {
         requirementAbbreviations[i].classList.remove("hide");
       }
+    });
+
+    questionPreviewDivs[i].addEventListener("click", () => {
+      questionAnswerFull[i].classList.contains("row-closed") ? questionAnswerFull[i].classList.remove("row-closed") : questionAnswerFull[i].classList.add("row-closed");
+      openCloseQuestionFull(i);
+      extractPreviews[i].classList.remove("extract-previews-open");
+      questionIcons[i].style.fontSize = "1.8rem";
+
+      if (requirementAbbreviations[i]) {
+        requirementAbbreviations[i].classList.remove("hide");
+      }
+
     });
   }
 
@@ -136,43 +163,5 @@ window.addEventListener('DOMContentLoaded', (event) => {
       contractRequirementTip(i);
       shortQuestionTexts[i].classList.remove("hide");
     });
-
-    let offsets = [];
-    let scrollWidths = [];
-
-    for (let i = 0; i < questionPreviews.length; i++) {
-      offsets[i] = shortQuestionTexts[i].offsetWidth;
-      scrollWidths[i] = shortQuestionTexts[i].scrollWidth;
-
-      shortQuestionTexts[i].addEventListener("mouseover", () => {
-        // if (offsets[i] < scrollWidths[i]) {
-        //   extractPreviews[i].classList.add("extract-extra-margin");
-        // }
-      });
-
-      shortQuestionTexts[i].addEventListener("mouseleave", () => {
-        extractPreviews[i].classList.remove("extract-extra-margin");
-      });
-
-      questionPreviewDivs[i].addEventListener("click", () => {
-        questionAnswerFull[i].classList.contains("row-closed") ? questionAnswerFull[i].classList.remove("row-closed") : questionAnswerFull[i].classList.add("row-closed");
-      });
-
-      questionIcons[i].addEventListener("mouseover", () => {
-        extractPreviews[i].classList.add("extract-previews-open");
-        questionIcons[i].style.fontSize = "3rem";
-        if (requirementAbbreviations[i]) {
-          requirementAbbreviations[i].classList.add("hide");
-        }
-      });
-
-      questionPreviews[i].addEventListener("mouseleave", () => {
-        extractPreviews[i].classList.remove("extract-previews-open");
-        questionIcons[i].style.fontSize = "1.8rem";
-        if (requirementAbbreviations[i]) {
-          requirementAbbreviations[i].classList.remove("hide");
-        }
-      });
-    }
   }
 });
