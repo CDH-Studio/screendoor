8bc69540f149ce27ff870757eaa968b3b6269135
Sam Heaton <heat0072@algonquinlive.com>
2019-07-29 10:30:26 -0400

    Arrows on applicant view now indicate expand/collapse elements

--- screendoor/models.py
+++ screendoor/models.py
@@ -17,8 +17,10 @@ class Position(models.Model):
     open_to = models.TextField(blank=True)
     reference_number = models.CharField(max_length=200, blank=True)
     selection_process_number = models.CharField(max_length=200, blank=True)
-    pdf = models.FileField(upload_to="positions/", validators=[FileExtensionValidator(allowed_extensions=['pdf'])],
-                           blank=True)
+    pdf = models.FileField(
+        upload_to="positions/",
+        validators=[FileExtensionValidator(allowed_extensions=['pdf'])],
+        blank=True)
     url_ref = models.URLField(max_length=200, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -32,47 +34,52 @@ class Position(models.Model):
     def update_applicant_fields(self):
         if self.applicant_set.all().count() > 0:
             self.number_applicants = self.applicant_set.all().count()
-            self.mean_score = sum([FormAnswer.objects.filter(parent_applicant=applicant, applicant_answer=True).count(
-            ) * 100 // FormAnswer.objects.filter(parent_applicant=applicant).count() for applicant in self.applicant_set.all()]) // self.applicant_set.all().count()
+            self.mean_score = sum([
+                FormAnswer.objects.filter(parent_applicant=applicant,
+                                          applicant_answer=True).count() *
+                100 //
+                FormAnswer.objects.filter(parent_applicant=applicant).count()
+                for applicant in self.applicant_set.all()
+            ]) // self.applicant_set.all().count()
             self.save()
 
 
 class Applicant(models.Model):
-    parent_position = models.ForeignKey(
-        Position, on_delete=models.CASCADE, null=True)
+    parent_position = models.ForeignKey(Position,
+                                        on_delete=models.CASCADE,
+                                        null=True)
     applicant_id = models.CharField(max_length=200, default='N/A')
 
     citizenship = models.CharField(max_length=200, null=True)
     priority = models.BooleanField(null=True)
     veteran_preference = models.BooleanField(null=True)
 
-    LANGUAGE_CHOICES = [
-        ('FR', 'French'),
-        ('EN', 'English'),
-        ('FREN', 'French or English')
-    ]
+    LANGUAGE_CHOICES = [('FR', 'French'), ('EN', 'English'),
+                        ('FREN', 'French or English')]
 
-    LANGUAGE_PROFICIENCY_CHOICES = [
-        ('NA', 'None'),
-        ('BEG', 'Beginner'),
-        ('INT', 'Intermediate'),
-        ('ADV', 'Advanced')
-    ]
+    LANGUAGE_PROFICIENCY_CHOICES = [('NA', 'None'), ('BEG', 'Beginner'),
+                                    ('INT', 'Intermediate'),
+                                    ('ADV', 'Advanced')]
     french_working_ability = models.CharField(
         choices=LANGUAGE_PROFICIENCY_CHOICES, max_length=200, null=True)
     english_working_ability = models.CharField(
         choices=LANGUAGE_PROFICIENCY_CHOICES, max_length=200, null=True)
-    first_official_language = models.CharField(
-        choices=LANGUAGE_CHOICES, max_length=200, null=True)
-    written_exam = models.CharField(
-        choices=LANGUAGE_CHOICES, max_length=200, null=True)
-    correspondence = models.CharField(
-        choices=LANGUAGE_CHOICES, max_length=200, null=True)
-    interview = models.CharField(
-        choices=LANGUAGE_CHOICES, max_length=200, null=True)
+    first_official_language = models.CharField(choices=LANGUAGE_CHOICES,
+                                               max_length=200,
+                                               null=True)
+    written_exam = models.CharField(choices=LANGUAGE_CHOICES,
+                                    max_length=200,
+                                    null=True)
+    correspondence = models.CharField(choices=LANGUAGE_CHOICES,
+                                      max_length=200,
+                                      null=True)
+    interview = models.CharField(choices=LANGUAGE_CHOICES,
+                                 max_length=200,
+                                 null=True)
 
-    pdf = models.FileField(upload_to="applications/", validators=[
-        FileExtensionValidator(allowed_extensions=['pdf'])],
+    pdf = models.FileField(
+        upload_to="applications/",
+        validators=[FileExtensionValidator(allowed_extensions=['pdf'])],
         blank=True)
     ranking = models.PositiveIntegerField(null=True)
     # For sorting purposes
@@ -95,37 +102,44 @@ class Applicant(models.Model):
             parent_applicant=self).count()
         classifications = list(
             Classification.objects.filter(parent_applicant=self))
-        self.classification_names = "".join([(classification.classification_substantive or "") for classification in classifications]).join(
-            " ").join([(classification.classification_current or "") for classification in classifications])
-
-
+        self.classification_names = "".join([
+            (classification.classification_substantive or "")
+            for classification in classifications
+        ]).join(" ").join([(classification.classification_current or "")
+                           for classification in classifications])
 
 
 class ScreenDoorUser(AbstractUser):
     email_confirmed = models.BooleanField(default=False)
-    positions = models.ManyToManyField(Position, blank=True, related_name='position_users')
+    positions = models.ManyToManyField(Position,
+                                       blank=True,
+                                       related_name='position_users')
     favourites = models.ManyToManyField(Applicant, blank=True)
+
     def confirm_email(self):
         self.email_confirmed = True
 
 
 class EmailAuthenticateToken(models.Model):
-    user = models.OneToOneField(
-        get_user_model(), on_delete=models.CASCADE, primary_key=False)
+    user = models.OneToOneField(get_user_model(),
+                                on_delete=models.CASCADE,
+                                primary_key=False)
     key = models.CharField(max_length=500, null=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def create_key(self):
         initial_key = Fernet.generate_key()
-        byte_values = bytes(str(self.user.email) +
-                            str(datetime.datetime.now()), 'utf-8')
+        byte_values = bytes(
+            str(self.user.email) + str(datetime.datetime.now()), 'utf-8')
         encoded_bytes = Fernet(initial_key).encrypt(byte_values)
         self.key = base64.b64encode(encoded_bytes).decode('utf-8')
 
 
 class RequirementMet(models.Model):
-    parent_applicant = models.ForeignKey(
-        Applicant, on_delete=models.CASCADE, null=True, related_name='requirementsmet')
+    parent_applicant = models.ForeignKey(Applicant,
+                                         on_delete=models.CASCADE,
+                                         null=True,
+                                         related_name='requirementsmet')
     requirement_type = models.CharField(max_length=200)
     abbreviation = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
@@ -136,8 +150,10 @@ class RequirementMet(models.Model):
 
 
 class Stream(models.Model):
-    parent_applicant = models.ForeignKey(
-        Applicant, on_delete=models.CASCADE, null=True, related_name='streams')
+    parent_applicant = models.ForeignKey(Applicant,
+                                         on_delete=models.CASCADE,
+                                         null=True,
+                                         related_name='streams')
     stream_name = models.CharField(max_length=200, null=True)
     stream_response = models.BooleanField(null=True)
     stream_description = models.CharField(max_length=200, null=True)
@@ -147,8 +163,10 @@ class Stream(models.Model):
 
 
 class Classification(models.Model):
-    parent_applicant = models.ForeignKey(
-        Applicant, on_delete=models.CASCADE, null=True, related_name='classifications')
+    parent_applicant = models.ForeignKey(Applicant,
+                                         on_delete=models.CASCADE,
+                                         null=True,
+                                         related_name='classifications')
 
     classification_substantive = models.CharField(max_length=200, null=True)
     classification_current = models.CharField(max_length=200, null=True)
@@ -158,8 +176,10 @@ class Classification(models.Model):
 
 
 class Education(models.Model):
-    parent_applicant = models.ForeignKey(
-        Applicant, on_delete=models.CASCADE, null=True, related_name='educations')
+    parent_applicant = models.ForeignKey(Applicant,
+                                         on_delete=models.CASCADE,
+                                         null=True,
+                                         related_name='educations')
     academic_level = models.TextField(null=True)
     area_of_study = models.TextField(null=True)
     specialization = models.TextField(null=True)
@@ -173,8 +193,7 @@ class Education(models.Model):
 
 
 class Requirement(models.Model):
-    position = models.ForeignKey(
-        Position, on_delete=models.CASCADE, null=True)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, null=True)
     requirement_type = models.CharField(max_length=200)
     abbreviation = models.CharField(max_length=200)
     description = models.TextField(blank=True)
@@ -184,10 +203,14 @@ class Requirement(models.Model):
 
 
 class FormQuestion(models.Model):
-    parent_position = models.ForeignKey(
-        Position, on_delete=models.CASCADE, null=True, related_name='questions')
-    parent_requirement = models.ForeignKey(
-        Requirement, on_delete=models.CASCADE, null=True, related_name='parent_req')
+    parent_position = models.ForeignKey(Position,
+                                        on_delete=models.CASCADE,
+                                        null=True,
+                                        related_name='questions')
+    parent_requirement = models.ForeignKey(Requirement,
+                                           on_delete=models.CASCADE,
+                                           null=True,
+                                           related_name='parent_req')
     question_text = models.TextField(blank=True, null=True)
     short_question_text = models.TextField(blank=True, null=True)
     complementary_question_text = models.TextField(blank=True, null=True)
@@ -197,61 +220,63 @@ class FormQuestion(models.Model):
 
 
 class FormAnswer(models.Model):
-    parent_question = models.ForeignKey(
-        FormQuestion, on_delete=models.CASCADE, null=True, related_name='answer')
-    parent_applicant = models.ForeignKey(
-        Applicant, on_delete=models.CASCADE, null=True, related_name='answers')
+    parent_question = models.ForeignKey(FormQuestion,
+                                        on_delete=models.CASCADE,
+                                        null=True,
+                                        related_name='answer')
+    parent_applicant = models.ForeignKey(Applicant,
+                                         on_delete=models.CASCADE,
+                                         null=True,
+                                         related_name='answers')
 
     applicant_answer = models.BooleanField()
-    applicant_complementary_response = models.TextField(
-        blank=True, null=True)
+    applicant_complementary_response = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return str(self.applicant_answer) + ": " + str(self.parent_question)
 
 
 class Qualifier(models.Model):
-    QUALIFIER_TYPES = [
-        ('RECENCY', 'Recency'),
-        ('SIGNIFICANCE', 'Significance')
-    ]
-    RESULT = [
-        ('PASS', "Passed"),
-        ('IND', "Indeterminate"),
-        ('FAIL', "Fail")
-    ]
+    QUALIFIER_TYPES = [('RECENCY', 'Recency'),
+                       ('SIGNIFICANCE', 'Significance')]
+    RESULT = [('PASS', "Passed"), ('IND', "Indeterminate"), ('FAIL', "Fail")]
 
-    parent_answer = models.ForeignKey(
-        FormAnswer, on_delete=models.CASCADE, null=True, related_name='answers')
-    qualifier_text = models.TextField(
-        blank=True, null=True)
-    qualifier_type = models.CharField(
-        choices=QUALIFIER_TYPES, max_length=200, null=True)
-    status = models.CharField(
-        choices=RESULT, max_length=200, null=True)
+    parent_answer = models.ForeignKey(FormAnswer,
+                                      on_delete=models.CASCADE,
+                                      null=True,
+                                      related_name='answers')
+    qualifier_text = models.TextField(blank=True, null=True)
+    qualifier_type = models.CharField(choices=QUALIFIER_TYPES,
+                                      max_length=200,
+                                      null=True)
+    status = models.CharField(choices=RESULT, max_length=200, null=True)
 
     def __str__(self):
         return str(self.qualifier_text)
 
 
 class Note(models.Model):
-    author = models.ForeignKey(
-        get_user_model(), on_delete=models.CASCADE, related_name='note')
-    parent_answer = models.ForeignKey(
-        FormAnswer, on_delete=models.CASCADE, null=True, related_name='note')
+    author = models.ForeignKey(get_user_model(),
+                               on_delete=models.CASCADE,
+                               related_name='note')
+    parent_answer = models.ForeignKey(FormAnswer,
+                                      on_delete=models.CASCADE,
+                                      null=True,
+                                      related_name='note')
     note_text = models.TextField(blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
 
 
 class NlpExtract(models.Model):
-    EXTRACT_TYPES = [
-        ('WHEN', 'A date or date range, and its context'),
-        ('HOW', 'What applicant did to fulfill a requirement')
-    ]
-    parent_answer = models.ForeignKey(
-        FormAnswer, on_delete=models.CASCADE, null=True, related_name='extract')
-    extract_type = models.CharField(
-        choices=EXTRACT_TYPES, max_length=200, null=True)
+    EXTRACT_TYPES = [('WHEN', 'A date or date range, and its context'),
+                     ('HOW', 'What applicant did to fulfill a requirement')]
+    parent_answer = models.ForeignKey(FormAnswer,
+                                      on_delete=models.CASCADE,
+                                      null=True,
+                                      related_name='extract')
+    extract_type = models.CharField(choices=EXTRACT_TYPES,
+                                    max_length=200,
+                                    null=True)
     extract_text = models.TextField()
     extract_sentence_index = models.PositiveIntegerField(null=True)
     extract_ending_index = models.PositiveIntegerField(null=True)
