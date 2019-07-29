3dce1def9c2e5124ff1d006c9760bb73ce14efed
Jared Ridyard <46499132+HeyaImPrizm@users.noreply.github.com>
2019-07-29 11:34:41 -0400

    Notes refactor (#111)
    * add eslint to gitignore
    
    * Refactor notes.js to ajax and fix javascript styling
    
    * remove console logs and removed statements

--- static/js/notes.js
+++ static/js/notes.js
@@ -1,34 +1,19 @@
-const addNoteButtons = document.getElementsByClassName("add-note");
-const addNoteForms = document.getElementsByClassName("note-form");
+const addNoteButtons = document.getElementsByClassName('add-note');
 
-const deleteNoteButtons = document.getElementsByClassName("delete-note");
-const deleteNoteForms = document.getElementsByClassName("delete-note-form");
+const cancelNoteButtons = document.getElementsByClassName('cancel-note');
 
-const cancelNoteButtons = document.getElementsByClassName("cancel-note");
-
-const noteAreas = document.getElementsByClassName("notes-area");
-const noteDeleteAnswerCounters = document.getElementsByClassName("note-delete-answer-counter");
 const noteInputs = document.getElementsByClassName('note-input');
-const noteRows = document.getElementsByClassName("notes-row");
 const noteTextArea = document.getElementsByClassName('note-box');
 
-const saveNoteButtons = document.getElementsByClassName("save-note");
-
-const persistOpenedQuestion = function(i) {
-  localStorage.setItem('questionIndex', i);
-};
-
-const persistOpenedEditBox = function(i) {
-  localStorage.setItem('boxOpen', i);
-};
+const saveNoteButtons = document.getElementsByClassName('save-note');
 
 const toggleNoteInput = function(i) {
-  if (noteTextArea[i].classList.contains("note-box-visible")) {
-    noteTextArea[i].classList.remove("note-box-visible");
-    addNoteButtons[i].children[0].style.fontSize = "2.1rem";
+  if (noteTextArea[i].classList.contains('note-box-visible')) {
+    noteTextArea[i].classList.remove('note-box-visible');
+    addNoteButtons[i].children[0].style.fontSize = '2.1rem';
   } else {
-    noteTextArea[i].classList.add("note-box-visible");
-    addNoteButtons[i].children[0].style.fontSize = "3rem";
+    noteTextArea[i].classList.add('note-box-visible');
+    addNoteButtons[i].children[0].style.fontSize = '3rem';
   }
 };
 
@@ -43,19 +28,75 @@ const retrieveOpenedQuestion = function() {
   }
 };
 
-const saveNote = function(i) {
-  if (addNoteForms[i].reportValidity()) {
-    persistOpenedQuestion(i);
-    persistOpenedEditBox(i);
-    persistScrollLocation();
-    addNoteForms[i].submit();
+const saveNote = function(i, answerNum) {
+  console.log(answerNum);
+  const noteInputId = 'note-input-' + i;
+  const noteInputField = document.getElementById(noteInputId);
+  const answerId = noteInputField.dataset.parentAnswer;
+  const url = '/add_note?noteText=' + noteInputField.value +
+    '&parentAnswerId=' + answerId;
+
+  fetch(url).then(function(response) {
+    /* data being the json object returned from Django function */
+    response.json().then(function(data) {
+      // Reformat the date string to accord with the template default
+      const options = {day: '2-digit', year: 'numeric',
+        month: 'long', hour: 'numeric', minute: 'numeric'};
+      const rawDateCreated = new Date(data.noteCreated);
+      const dateCreated = rawDateCreated.toLocaleDateString('en-US', options);
+      const datestring = dateCreated.replace('AM', 'a.m.')
+          .replace('PM', 'p.m.');
+
+      // Create note DOM element
+      note = document.createElement('div');
+      note.classList.add('note');
+      note.id = data.noteId;
+      note.setAttribute('data-parent-answer', answerId);
+      note.innerHTML = '<span class="note-text">' + data.noteText + '</span>';
+      note.innerHTML += ('<br>');
+      note.innerHTML += ('<span class="note-author grey-text">' +
+        data.noteAuthor + ', </span><span class="note-created grey-text">' +
+        datestring + '</span>');
+      note.innerHTML += ('<i class="material-icons red-text delete-note"' +
+        'data-note-id="' + data.noteId + '" data-answer-num="' + answerNum +
+        '">delete_forever</i>');
+
+      // Add the new element to its holder block
+      const notesBlockId = 'notes-' + answerNum;
+      const notesBlock = document.getElementById(notesBlockId);
+      notesBlock.insertBefore(note, notesBlock.childNodes[0]);
+
+      // Add event handler to new remove button
+      addRemoveNoteHandlers();
+    }).catch((error) => console.error());
+  });
+};
+
+const addRemoveNoteHandlers = function() {
+  const removeNoteButtons = document.getElementsByClassName('delete-note');
+  for (let i = 0; i < removeNoteButtons.length; i++) {
+    const noteId = removeNoteButtons[i].dataset.noteId;
+    const answerNum = removeNoteButtons[i].dataset.answerNum;
+    removeNoteButtons[i].addEventListener('click', () => {
+      removeNote(noteId, answerNum);
+    });
   }
 };
 
-const deleteNote = function(i) {
-  persistOpenedQuestion(noteDeleteAnswerCounters[i].value);
-  persistScrollLocation();
-  deleteNoteForms[i].submit();
+const removeNote = function(noteId, answerNum) {
+  const url = '/remove_note?noteId=' + noteId;
+  fetch(url).then(function(response) {
+    /* data being the json object returned from Django function */
+    response.json().then(function(data) {
+      // retrieve the block holding the note
+      const notesBlockId = 'notes-' + answerNum;
+      const notesBlock = document.getElementById(notesBlockId);
+
+      // find and remove the note from the block
+      const note = document.getElementById(noteId);
+      notesBlock.removeChild(note);
+    }).catch((error) => console.error());
+  });
 };
 
 const cancelAddNote = function(i) {
@@ -67,15 +108,22 @@ window.addEventListener('DOMContentLoaded', function() {
   getScrollLocation();
 
   for (let i = 0; i < addNoteButtons.length; i++) {
+    i % 2 === 0 ? noteInputs[i].style.backgroundColor = '#ffffff' :
+      noteInputs[i].style.backgroundColor = '#fafafa';
 
-    i % 2 === 0 ? noteInputs[i].style.backgroundColor = "#ffffff" : noteInputs[i].style.backgroundColor = "#fafafa";
+    addNoteButtons[i].addEventListener('click', () => {
+      toggleNoteInput(i);
+    });
 
-    addNoteButtons[i].addEventListener("click", () => { toggleNoteInput(i); });
-    cancelNoteButtons[i].addEventListener("click", () => { cancelAddNote(i); });
-    saveNoteButtons[i].addEventListener("click", () => { saveNote(i); });
+    cancelNoteButtons[i].addEventListener('click', () => {
+      cancelAddNote(i);
+    });
+
+    const answerNum = saveNoteButtons[i].dataset.answerNum;
+    saveNoteButtons[i].addEventListener('click', () => {
+      saveNote(i, answerNum);
+    });
   }
 
-  for (let i = 0; i < deleteNoteButtons.length; i++) {
-    deleteNoteButtons[i].addEventListener("click", () => { deleteNote(i); });
-  }
+  addRemoveNoteHandlers();
 });
